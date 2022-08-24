1f592c657cc0d743b2e5a0e30d9ce5ef3e436a2e
Ginevrana <florenciateresita@gmail.com>
2022-08-23 23:11:44 -0300

    Tercer punto de ejercitacion: inputs

--- ej1.py
+++ ej1.py
@@ -12,3 +12,10 @@ mundo = "World!"
 
 saludar = (saludo + " " + mundo)
 print(saludar)
+
+
+# Ejercicio 3
+nombre = input("Ingrese su nombre:")
+dni = input("Ingrese su DNI:")
+
+print(f'El nombre del usuario es {nombre} y su DNI es {dni}')
