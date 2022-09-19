796892efe6e135210d6148321f2cfa8bb36c653c
Ginevrana <florenciateresita@gmail.com>
2022-09-19 00:17:32 -0300

    Apuntes de clase 13-09

--- clase4.py
+++ clase4.py
@@ -21,10 +21,10 @@
 # MATRICES O ARREGLOS DE DOS DIMENSIONES
 # Es una lista cuyos elementos tienen la misma longitud  y contienen elementos del mismo tipo
 
-a=[[23,45,63],[72,81,91],[56,64,37],[34,75,26]]
+#a=[[23,45,63],[72,81,91],[56,64,37],[34,75,26]]
 
 # Para acceder a la primer lista
-print(a[1])
+#print(a[1])
 # Resultado [72, 81, 91]
 
 # # Para acceder a una posición específica
@@ -46,3 +46,61 @@ print(a[1])
 # print(mul[2][1][0])
 # # Resultado: 333
 
+# -------------------------------------------------------------------------------------------------
+
+# x=[5,23,46,85,93,112]
+# e=18
+
+# Agregar a la lista el elemento
+# x.append(e)
+
+# Insertar el elemento en una posición específica
+# x.insert(2,e)
+# print(x)
+
+# Conteo de instancia de elemento en lista
+# print(x.count(e))
+
+# Eliminar el primer elemento indicado de la lista
+# x.remove(e)
+
+# Entrega la posición del primer elemento indicado en la lista
+# print(x.index(e))
+
+# Para ordenar en forma creciente
+#x.sort()
+
+# Invierte la lista
+#x.reverse()
+
+# Vacía la lista
+# x.clear()
+# print(x)
+
+
+# -----------------------------------------------------------------------------------------------------
+# LISTAS
+# Operadores y funciones adicionales para listas
+
+# Concatenar listas
+# l=[1,2,3]
+# print(l)
+
+# l+=[4,5]
+# print(l)
+
+# Operadores de inclusión para saber si un elemento está en una lista
+# vocales = ['a','e','i','o','u']
+# if 'a' in vocales:
+    # print("Sí")
+
+# if 'b' not in vocales:
+    # print('No')
+
+# Eliminar elementos de una lista especificando el índice
+# del vocales[1]
+# print(vocales)
+
+#---------------------------------------------------------------------------------------------------
+# DICCIONARIOS
+# Están constituidos por elementos de la forma llave-valor y se accede a un elemento mediante su llave. 
