39966409f24b2f751c217bf87e637fcf0eb5c11c
Ginevrana <florenciateresita@gmail.com>
2022-09-18 22:40:05 -0300

    Se realiza ejercicio 2 de funciones

--- funciones.py
+++ funciones.py
@@ -4,3 +4,11 @@ def area_rectangulo(x,y):
     return x*y
 
 print(f'El área del rectangulo es: {area_rectangulo(5,2)}')
+
+# Ejercicio 2 - Realizar función que se llame area_circulo()
+
+def area_circulo(radio):
+    pi= 3.14159
+    return pi * (radio ** 2)
+
+print(f'El área del circulo es: {area_circulo(2)}')
