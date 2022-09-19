74672e7dcad06526dfb89e57c724901043c21b25
Ginevrana <florenciateresita@gmail.com>
2022-09-18 22:43:49 -0300

    Se realiza ejercicio 3 de funciones

--- funciones.py
+++ funciones.py
@@ -12,3 +12,15 @@ def area_circulo(radio):
     return pi * (radio ** 2)
 
 print(f'El área del circulo es: {area_circulo(2)}')
+
+# Ejercicio 3 - Realizar función que se llame relación
+
+def relacion(x,y):
+    if x>y:
+        return 1
+    elif x<y:
+        return -1
+    elif x==y:
+        return 0
+
+print(relacion(5,5))
